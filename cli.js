29097fed5f8dd91346b80df24e5a23359ac45492
Arnaldo Neto <costanetoow@gmail.com>
2022-04-02 17:23:40 -0300

    used fetch to deal with http

--- cli.js
+++ cli.js
@@ -7,7 +7,7 @@ const path = process.argv;
 async function processText(filePath) {
   const result = await getFile(filePath[2]);
   if (path[3] === "validate") {
-    console.log(chalk.yellow("Valaidated links"), validateUrl(result));
+    console.log(chalk.yellow("Valaidated links"), await validateUrl(result));
   } else {
     console.log(chalk.yellow("Links list:"), result);
   }
--- http-validation.js
+++ http-validation.js
@@ -1,7 +1,21 @@
+import fetch from "node-fetch";
+
+async function checkStatus(urlArray) {
+  const statusArray = await Promise.all(
+    urlArray.map(async (url) => {
+      const res = await fetch(url);
+      return res.status;
+    })
+  );
+  return statusArray;
+}
+
 function createUrlsArray(linksArray) {
   return linksArray.map((linkObject) => Object.values(linkObject).join());
 }
 
-export default function validateUrl(linksArray) {
-  return createUrlsArray(linksArray);
+export default async function validateUrl(linksArray) {
+  const links = createUrlsArray(linksArray);
+  const linksStatus = await checkStatus(links);
+  return linksStatus;
 }
