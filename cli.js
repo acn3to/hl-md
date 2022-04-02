8166740ecc36b8df7aff21c0b688ab241d84c9bc
Arnaldo Neto <costanetoow@gmail.com>
2022-04-02 13:02:41 -0300

    organized data input and output

--- cli.js
+++ cli.js
@@ -1,5 +1,11 @@
+import chalk from "chalk";
 import getFile from "./index.js";
 
 const path = process.argv;
 
-console.log(getFile(path[2]));
+async function processText(filePath) {
+  const result = await getFile(filePath[2]);
+  console.log(chalk.yellow("Links list:"), result);
+}
+
+processText(path);
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function getLink(text) {
   while ((temp = regex.exec(text)) != null) {
     resultsArray.push({ [temp[1]]: temp[2] });
   }
-  return resultsArray;
+  return resultsArray.length === 0 ? "There is no links" : resultsArray;
 }
 
 function handleError(err) {
@@ -20,12 +20,10 @@ export default async function getFile(filePath) {
   try {
     const encoding = "utf-8";
     const text = await fs.promises.readFile(filePath, encoding);
-    console.log(getLink(text));
+    return getLink(text);
   } catch (err) {
     handleError(err);
   } finally {
     console.log(chalk.yellow("Job done!"));
   }
 }
-
-// getFile("./files/markdown-syntax.md");
